f2f60709551faa71f76f74d0736b3a4bb7fcf650
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-18 15:08:16 +0300

    complete cf lab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+const heightOfride = '';
+let freeRide, twentyBuckfee, thirtyBuckfee, noRide;
+
+function scuberGreetingForFeet(heightOfride){
   // Write your code here!
+  if (heightOfride <= 400) {
+    freeRide = 'This one is on me!';
+    return freeRide
+  } 
+  else if (heightOfride > 400 && heightOfride < 2000) {
+      twentyBuckfee = 'That will be twenty bucks.';
+      return twentyBuckfee
+  }
+  else if (heightOfride > 2000 && heightOfride < 2500) {
+      thirtyBuckfee = 'I will gladly take your thirty bucks.';
+      return thirtyBuckfee
+  }
+  else if (heightOfride > 2500) {
+      noRide = 'No can do.';
+      return noRide
+  }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') {
+    return 'Thank you so much.'
+  }
+  else if (tip === 'not as generous') {
+    return 'Thank you.'
+  }
+  else {
+    return 'Bye.'
+  }
 }
